ae153a48cf2caa23563b8b8e77078671c0ed7ca7
Okikioluwa Onamade <khodedng@yahoo.com>
2020-02-02 02:05:24 +0100

    Update index.js

--- pkg/movies/index.js
+++ pkg/movies/index.js
@@ -2,7 +2,7 @@ const moviesDelivery = require('./delivery/http/routes/routes')
 const repository = require('./repository/lotr_repo');
 const usecase = require('./usecase/usecase');
 
-//register news service
+//register movies service
 const repoInstance = new repository()
 const usecaseInstance = new usecase(repoInstance)
 moviesDelivery.moviesDelivery(usecaseInstance)
