ae9c049bcd3487469e6cab48c842ee43bce7084f
Okikioluwa Onamade <khodedng@yahoo.com>
2020-02-02 02:06:05 +0100

    Update index.js

--- pkg/characters/index.js
+++ pkg/characters/index.js
@@ -2,7 +2,7 @@ const characterDelivery = require('./delivery/http/routes/routes')
 const repository = require('./repository/lotr_repo');
 const usecase = require('./usecase/usecase');
 
-//register news service
+//register character service
 const repoInstance = new repository()
 const usecaseInstance = new usecase(repoInstance)
 characterDelivery.characterDelivery(usecaseInstance)
